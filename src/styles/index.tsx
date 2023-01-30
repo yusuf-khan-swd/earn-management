4fac2afe707122a92d04c5db243b08f32dc6c98a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-30 22:43:19 +0600

    format with prettier code

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import btnPng from '../assets/btnPng.png';
-import logo from '../assets/logo.png';
-import bannerImage from '../assets/banner.png';
-import discordImage from '../assets/discord.png';
-import twitterImage from '../assets/twitter.png';
+import btnPng from "../assets/btnPng.png";
+import logo from "../assets/logo.png";
+import bannerImage from "../assets/banner.png";
+import discordImage from "../assets/discord.png";
+import twitterImage from "../assets/twitter.png";
 
 export const Navbar = styled.nav`
   font-size: 15px;
@@ -12,29 +12,29 @@ export const Navbar = styled.nav`
   align-items: center;
   padding-top: 8px;
   margin: 0 auto;
-`
+`;
 
 export const NavLogo = styled.img.attrs(() => ({
-  src: logo
+  src: logo,
 }))`
   max-width: 70px;
   margin: 8px;
-`
+`;
 
 export const MenuContainer = styled.ul`
   list-style: none;
   display: flex;
-`
+`;
 export const MenuItems = styled.li`
   margin-right: 31px;
 `;
 
 export const MenuItemsLink = styled.a.attrs(() => ({
-  href: "/#"
+  href: "/#",
 }))`
   color: white;
   text-decoration: none;
-  `
+`;
 
 export const NavSocialMediaContainer = styled.div`
   border: 1px solid rgba(255, 255, 255, 0.17);
@@ -47,35 +47,35 @@ export const NavSocialMediaContainer = styled.div`
 `;
 
 export const DiscordLink = styled.a.attrs(() => ({
-  href: '#'
+  href: "#",
 }))`
   font-size: 20px;
   margin-right: 29px;
   color: white;
-`
+`;
 
 export const Discord = styled.img.attrs(() => ({
-  src: discordImage
-}))``
+  src: discordImage,
+}))``;
 
 export const TwitterLink = styled.a.attrs(() => ({
-  href: '#'
+  href: "#",
 }))`
   font-size: 20px;
   color: white;
-`
+`;
 export const Twitter = styled.img.attrs(() => ({
-  src: twitterImage
-}))``
+  src: twitterImage,
+}))``;
 
 export const Banner = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  
+
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
   }
-`
+`;
 
 export const BannerContentSection = styled.div``;
 export const BannerTitle = styled.h1`
@@ -100,15 +100,15 @@ export const InputContainer = styled.div`
 `;
 
 export const InputEmail = styled.input.attrs(() => ({
-  type: 'email',
-  placeholder: 'Enter you Email Address'
+  type: "email",
+  placeholder: "Enter you Email Address",
 }))`
   border: none;
   padding: 12px 0px;
   width: 60%;
   background: transparent;
   margin-left: 4px;
-`
+`;
 
 export const Select = styled.select`
   background: transparent;
@@ -118,39 +118,48 @@ export const Select = styled.select`
   option: {
     margin-left: 10px;
   }
-  
-`
+`;
 export const SelectOption = styled.option.attrs(() => ({
-  value: "0"
-}))``
+  value: "0",
+}))``;
 
 export const ButtonIcon = styled.img.attrs(() => ({
-  src: btnPng
+  src: btnPng,
 }))`
   max-width: 50px;
   margin-right: 2px;
-`
+`;
 
 export const BannerImageSection = styled.div`
-  background: radial-gradient(41.76% 41.76% at 50% 50%, rgba(0, 209, 255, 0.24) 0%, rgba(0, 163, 255, 0) 100%);
+  background: radial-gradient(
+    41.76% 41.76% at 50% 50%,
+    rgba(0, 209, 255, 0.24) 0%,
+    rgba(0, 163, 255, 0) 100%
+  );
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 export const BannerImage = styled.img.attrs(() => ({
-  src: bannerImage
+  src: bannerImage,
 }))`
   width: 100%;
-`
+`;
 
 export const RoadMapContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   border: 3px solid rgba(255, 255, 255, 0.12);
-`
+`;
 
 export const RoadMapContent = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`
+`;
+
+export const RoadMapTitle = styled.div`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 80px;
+`;
