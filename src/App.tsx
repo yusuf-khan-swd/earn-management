efe2e054ed61a038113f7d5898a6858aedd95b2d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-31 18:09:37 +0600

    linear background color problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import Home from './components/Home/Home';
+import Home from "./components/Home/Home";
 
 function App() {
-
   return (
     <div>
       <Home></Home>
     </div>
-  )
+  );
 }
 
 export default App;
