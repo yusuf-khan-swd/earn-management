0f9d51eef3c001fc04bde041b58bffcb557a1864
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-17 23:03:05 +0600

    remove react fragment

--- src/components/RoadMap.tsx
+++ src/components/RoadMap.tsx
@@ -27,117 +27,115 @@ import primaryIcon from "../assets/images/primaryIcon.png";
 import secondaryIcon from "../assets/images/secondaryIcon.png";
 
 const RoadMap = () => (
-  <>
-    <RoadMapContainer>
-      <RoadMapLeftSide />
-      <RoadMapMainContainer>
-        <RoadMapMain>
-          <RoadMapContent>
-            <RoadMapTitleContainer>
-              <RoadMapTitle>Road Map</RoadMapTitle>
-              <RoadMapSubTitle>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </RoadMapSubTitle>
-            </RoadMapTitleContainer>
-            <RoadMapLeft border={false}>
-              <YearsRoadMapContent left={true}>
-                <YearsRoadMapTitle left={true}>2023</YearsRoadMapTitle>
-                <YearsRoadMapSubTitle left={true}>Q2</YearsRoadMapSubTitle>
-                <YearsRoadMapBody left={true}>
-                  <YearsRoadMapBodyText left={true}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Suspendisse id purus augue. Lorem ipsum dolor
-                  </YearsRoadMapBodyText>
-                </YearsRoadMapBody>
-              </YearsRoadMapContent>
-              <RoadMapImageContainer left={true}>
-                <RoadMapImage src={primaryIcon} alt="icon"></RoadMapImage>
-              </RoadMapImageContainer>
-            </RoadMapLeft>
-          </RoadMapContent>
-          <RoadMapMainImageContainer>
-            <RoadMapMainImage src={roadMapMainImage} alt="icon" />
-          </RoadMapMainImageContainer>
-        </RoadMapMain>
-        <RoadMapRight>
-          <YearsRoadMapContent left={false}>
-            <YearsRoadMapTitle left={false}>2022</YearsRoadMapTitle>
-            <YearsRoadMapSubTitle left={false}>Q2</YearsRoadMapSubTitle>
-            <YearsRoadMapBody left={false}>
-              <YearsRoadMapBodyText left={false}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse id purus augue. Lorem ipsum dolor
-              </YearsRoadMapBodyText>
-            </YearsRoadMapBody>
-          </YearsRoadMapContent>
-          <RoadMapImageContainer left={false}>
-            <RoadMapImage src={secondaryIcon} alt="icon" />
-          </RoadMapImageContainer>
-        </RoadMapRight>
-        <RoadMapLeft border={true}>
-          <YearsRoadMapContent left={true}>
-            <YearsRoadMapTitle left={true}>2023</YearsRoadMapTitle>
-            <YearsRoadMapSubTitle left={true}>Q2</YearsRoadMapSubTitle>
-            <YearsRoadMapBody left={true}>
-              <YearsRoadMapBodyText left={true}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse id purus augue. Lorem ipsum dolor
-              </YearsRoadMapBodyText>
-            </YearsRoadMapBody>
-          </YearsRoadMapContent>
-          <RoadMapImageContainer left={true}>
-            <RoadMapImage src={secondaryIcon} alt="icon" />
-          </RoadMapImageContainer>
-        </RoadMapLeft>
-        <RoadMapRight>
-          <YearsRoadMapContent left={false}>
-            <YearsRoadMapTitle left={false}>2022</YearsRoadMapTitle>
-            <YearsRoadMapSubTitle left={false}>Q2</YearsRoadMapSubTitle>
-            <YearsRoadMapBody left={false}>
-              <YearsRoadMapBodyText left={false}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse id purus augue. Lorem ipsum dolor
-              </YearsRoadMapBodyText>
-            </YearsRoadMapBody>
-          </YearsRoadMapContent>
-          <RoadMapImageContainer left={false}>
-            <RoadMapImage src={secondaryIcon} alt="icon" />
-          </RoadMapImageContainer>
-        </RoadMapRight>
-        <RoadMapLeft border={true}>
-          <YearsRoadMapContent left={true}>
-            <YearsRoadMapTitle left={true}>2023</YearsRoadMapTitle>
-            <YearsRoadMapSubTitle left={true}>Q2</YearsRoadMapSubTitle>
-            <YearsRoadMapBody left={true}>
-              <YearsRoadMapBodyText left={true}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse id purus augue. Lorem ipsum dolor
-              </YearsRoadMapBodyText>
-            </YearsRoadMapBody>
-          </YearsRoadMapContent>
-          <RoadMapImageContainer left={true}>
-            <RoadMapImage src={secondaryIcon} alt="icon" />
-          </RoadMapImageContainer>
-        </RoadMapLeft>
-        <RoadMapRight>
-          <YearsRoadMapContent left={false}>
-            <YearsRoadMapTitle left={false}>2022</YearsRoadMapTitle>
-            <YearsRoadMapSubTitle left={false}>Q2</YearsRoadMapSubTitle>
-            <YearsRoadMapBody left={false}>
-              <YearsRoadMapBodyText left={false}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse id purus augue. Lorem ipsum dolor
-              </YearsRoadMapBodyText>
-            </YearsRoadMapBody>
-            <RoadMapImageContainer left={false}>
-              <RoadMapImage src={secondaryIcon} alt="icon" />
+  <RoadMapContainer>
+    <RoadMapLeftSide />
+    <RoadMapMainContainer>
+      <RoadMapMain>
+        <RoadMapContent>
+          <RoadMapTitleContainer>
+            <RoadMapTitle>Road Map</RoadMapTitle>
+            <RoadMapSubTitle>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </RoadMapSubTitle>
+          </RoadMapTitleContainer>
+          <RoadMapLeft border={false}>
+            <YearsRoadMapContent left={true}>
+              <YearsRoadMapTitle left={true}>2023</YearsRoadMapTitle>
+              <YearsRoadMapSubTitle left={true}>Q2</YearsRoadMapSubTitle>
+              <YearsRoadMapBody left={true}>
+                <YearsRoadMapBodyText left={true}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse id purus augue. Lorem ipsum dolor
+                </YearsRoadMapBodyText>
+              </YearsRoadMapBody>
+            </YearsRoadMapContent>
+            <RoadMapImageContainer left={true}>
+              <RoadMapImage src={primaryIcon} alt="icon"></RoadMapImage>
             </RoadMapImageContainer>
-          </YearsRoadMapContent>
-        </RoadMapRight>
-      </RoadMapMainContainer>
-      <RoadMapRightSide />
-    </RoadMapContainer>
-  </>
+          </RoadMapLeft>
+        </RoadMapContent>
+        <RoadMapMainImageContainer>
+          <RoadMapMainImage src={roadMapMainImage} alt="icon" />
+        </RoadMapMainImageContainer>
+      </RoadMapMain>
+      <RoadMapRight>
+        <YearsRoadMapContent left={false}>
+          <YearsRoadMapTitle left={false}>2022</YearsRoadMapTitle>
+          <YearsRoadMapSubTitle left={false}>Q2</YearsRoadMapSubTitle>
+          <YearsRoadMapBody left={false}>
+            <YearsRoadMapBodyText left={false}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse id purus augue. Lorem ipsum dolor
+            </YearsRoadMapBodyText>
+          </YearsRoadMapBody>
+        </YearsRoadMapContent>
+        <RoadMapImageContainer left={false}>
+          <RoadMapImage src={secondaryIcon} alt="icon" />
+        </RoadMapImageContainer>
+      </RoadMapRight>
+      <RoadMapLeft border={true}>
+        <YearsRoadMapContent left={true}>
+          <YearsRoadMapTitle left={true}>2023</YearsRoadMapTitle>
+          <YearsRoadMapSubTitle left={true}>Q2</YearsRoadMapSubTitle>
+          <YearsRoadMapBody left={true}>
+            <YearsRoadMapBodyText left={true}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse id purus augue. Lorem ipsum dolor
+            </YearsRoadMapBodyText>
+          </YearsRoadMapBody>
+        </YearsRoadMapContent>
+        <RoadMapImageContainer left={true}>
+          <RoadMapImage src={secondaryIcon} alt="icon" />
+        </RoadMapImageContainer>
+      </RoadMapLeft>
+      <RoadMapRight>
+        <YearsRoadMapContent left={false}>
+          <YearsRoadMapTitle left={false}>2022</YearsRoadMapTitle>
+          <YearsRoadMapSubTitle left={false}>Q2</YearsRoadMapSubTitle>
+          <YearsRoadMapBody left={false}>
+            <YearsRoadMapBodyText left={false}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse id purus augue. Lorem ipsum dolor
+            </YearsRoadMapBodyText>
+          </YearsRoadMapBody>
+        </YearsRoadMapContent>
+        <RoadMapImageContainer left={false}>
+          <RoadMapImage src={secondaryIcon} alt="icon" />
+        </RoadMapImageContainer>
+      </RoadMapRight>
+      <RoadMapLeft border={true}>
+        <YearsRoadMapContent left={true}>
+          <YearsRoadMapTitle left={true}>2023</YearsRoadMapTitle>
+          <YearsRoadMapSubTitle left={true}>Q2</YearsRoadMapSubTitle>
+          <YearsRoadMapBody left={true}>
+            <YearsRoadMapBodyText left={true}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse id purus augue. Lorem ipsum dolor
+            </YearsRoadMapBodyText>
+          </YearsRoadMapBody>
+        </YearsRoadMapContent>
+        <RoadMapImageContainer left={true}>
+          <RoadMapImage src={secondaryIcon} alt="icon" />
+        </RoadMapImageContainer>
+      </RoadMapLeft>
+      <RoadMapRight>
+        <YearsRoadMapContent left={false}>
+          <YearsRoadMapTitle left={false}>2022</YearsRoadMapTitle>
+          <YearsRoadMapSubTitle left={false}>Q2</YearsRoadMapSubTitle>
+          <YearsRoadMapBody left={false}>
+            <YearsRoadMapBodyText left={false}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse id purus augue. Lorem ipsum dolor
+            </YearsRoadMapBodyText>
+          </YearsRoadMapBody>
+          <RoadMapImageContainer left={false}>
+            <RoadMapImage src={secondaryIcon} alt="icon" />
+          </RoadMapImageContainer>
+        </YearsRoadMapContent>
+      </RoadMapRight>
+    </RoadMapMainContainer>
+    <RoadMapRightSide />
+  </RoadMapContainer>
 );
 
 export default RoadMap;
